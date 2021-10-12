46466cbbe4c869a865add1b3efd80f6826f99002
Chapalah <70241721+Chapalah@users.noreply.github.com>
2021-10-12 23:54:12 +0300

    Delete vitok directory

--- vitok/js/functions.js
+++
@@ -1,7 +0,0 @@
-const toggleClass = (item, ...classNames) => {
-    if (item.classList.contains(...classNames)) {
-        item.classList.remove(...classNames)
-    } else {
-        item.classList.add(...classNames)
-    }
-}
